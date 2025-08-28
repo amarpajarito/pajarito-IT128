7696113e797c599c353689c0ec73ef7878c783b3
amarpajarito <amarpajarito1@gmail.com>
2025-08-28 20:12:33 +0800

    Add PajaritoLE2 code

---
+++ PajaritoLE2/BlogDataLibrary/Database/ISqlDataAccess.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace BlogDataLibrary.Database
+{
+    public interface ISqlDataAccess
+    {
+        List<T> LoadData<T, U>(string sqlStatment, U parameters, string connectionStringName, bool isStoredProcedure);
+        void SaveData<T>(string sqlStatment, T parameters, string connectionStringName, bool isStoredProcedure);
+    }
+}
