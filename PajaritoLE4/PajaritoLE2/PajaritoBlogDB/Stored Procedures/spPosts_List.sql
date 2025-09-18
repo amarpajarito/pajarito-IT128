f3b76ebb1a8102e331c8437e0478428b512f52e6
amarpajarito <amarpajarito1@gmail.com>
2025-09-18 14:03:01 +0800

    Add PajaritoLE4 code

---
+++ PajaritoLE4/PajaritoLE2/PajaritoBlogDB/Stored Procedures/spPosts_List.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[spPosts_List]
+AS
+begin
+	set nocount on;
+
+	SELECT [p].[Id], [p].[Title], [p].[Body], [p].[DateCreated], [u].[UserName], [u].[firstName], [u].[LastName]
+	FROM dbo.Posts p
+	INNER JOIN dbo.Users u
+	On p.UserId = u.Id
+end
+RETURN 0
